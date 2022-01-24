98850c969070c385baabf6656224d1772bd91e9a
Rashad Long <RDLong718@gmail.com>
2022-01-24 14:51:35 -0500

    Started Planet Data Set
    PlanetName, imgURL, and description

---
+++ PlanetsData.js
@@ -0,0 +1,90 @@
+export default[ {
+        planetName: "Tatooine",
+        description: "A harsh desert world orbiting twin suns in the galaxy’s Outer Rim, Tatooine is a lawless place ruled by Hutt gangsters. Many settlers scratch out a living on moisture farms, while spaceport cities such as Mos Eisley and Mos Espa serve as home base for smugglers, criminals, and other rogues.",
+        imgURL:"https://upload.wikimedia.org/wikipedia/en/6/6d/Tatooine_%28fictional_desert_planet%29.jpg",
+
+},
+    {
+        planetName: "Coruscant",
+        description: "A city-covered planet, Coruscant is the vibrant heart and capital of the galaxy, featuring a diverse mix of citizens and culture. It features towering skyscrapers, streams of speeder-filled air traffic, and inner levels that stretch far below the world’s surface.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/1/16/Coruscant-EotE.jpg/revision/latest?cb=20130908205521"
+    },
+    {
+        planetName: "Hoth",
+        description: "Hoth is the sixth planet in the remote system of the same name, and was the site of the Rebel Alliance's Echo Base. It is a world of snow and ice, surrounded by numerous moons, and home to deadly creatures like the wampa.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/1/1d/Hoth_SWCT.png/revision/latest/scale-to-width-down/1000?cb=20160630022322"
+    },
+    {
+        planetName: "Naboo",
+        description: "An idyllic world close to the border of the Outer Rim Territories, Naboo is inhabited by peaceful humans known as the Naboo, and an indigenous species of intelligent amphibians called the Gungans. Naboo's surface consists of swampy lakes, rolling plains and green hills.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/f/f0/Naboo_planet.png/revision/latest?cb=20190210065915"
+    },
+    {
+        planetName: "Mustafar",
+        description: "A tiny, fiery planet in the Outer Rim, Mustafar maintains an erratic orbit between two gas giants. Mustafar is rich in unique and valuable minerals which have long been mined by the Tech Union.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/6/61/Mustafar-TROSGG.png/revision/latest/scale-to-width-down/1000?cb=20200722072411"
+    },
+    {
+        planetName: "Kamino",
+        description: "Purged from the otherwise complete Jedi Archives is all evidence of the mysterious world of Kamino. A lonely world beyond the Outer Rim and just south of the Rishi Maze, few could have predicted that Kamino would become a key contributor to a massive shift in political power in the final days of the Republic.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/a/a9/Eaw_Kamino.jpg/revision/latest?cb=20090527045541"
+    },
+    {
+        planetName: "Dagobah",
+        description: "Home to Yoda during his final years, Dagobah was a swamp-covered planet -- a forgotten world where the wizened Jedi Master could escape the notice of Imperial forces. Characterized by its bog-like conditions and fetid wetlands, the murky and humid quagmire was undeveloped, with no signs of technology.",
+        imgURL:"https://upload.wikimedia.org/wikipedia/en/1/1c/Dagobah.jpg"
+    },
+    {
+        planetName: "Kashyyyk",
+        description: "Kashyyyk is the Wookiee homeworld, covered in dense forest. While Wookiees build their homes in the planet's trees, they are not a primitive species, and Kashyyyk architecture incorporates sophisticated technology.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/e/ea/Kashyyyk-SW-MTHC.png/revision/latest/scale-to-width-down/700?cb=20190605010641"
+    },
+    {
+        planetName: "Bespin",
+        description: "Secluded from galactic turmoil by its location in a little-visited sector of space, Bespin is an astrophysical rarity. An immense gas giant surrounded by a number of moons, the planet contains a band of habitable atmosphere among its endless clouds. ",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/2/2c/Bespin_EotECR.png/revision/latest/scale-to-width-down/1000?cb=20170222012550"
+    },
+    {
+        planetName: "Alderaan",
+        description: "If ever one needed an example of the irredeemable evil that was the Empire, turn to the shattered remains of Alderaan. An influential world, Alderaan was represented in the waning days of the Republic by such venerated politicians as Bail Antilles and Bail Organa.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/4/4a/Alderaan.jpg/revision/latest/scale-to-width-down/1000?cb=20061211013805"
+    },
+    {
+        planetName: "Yavin",
+        description: "Yavin is a fictional planet in the Star Wars galaxy. It first appeared in the 1977 film Star Wars and is depicted as a large red gas giant with an extensive satellite system of moons. The hidden military base of the Rebel Alliance is located on its fourth moon, Yavin 4",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/d/d4/Yavin-4-SWCT.png/revision/latest/scale-to-width-down/1000?cb=20181015023938"
+    },
+    {
+        planetName: "Geonosis",
+        description: "A harsh rocky world less than a parsec away from Tatooine, Geonosis is a ringed planet beyond the borders of the Galactic Republic. Its uninviting surface is made up of harsh, rocky desert, and the creatures that evolved on Geonosis are well equipped to survive in the brutal environment.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/d/d4/Yavin-4-SWCT.png/revision/latest/scale-to-width-down/1000?cb=20181015023938"
+    },
+    {
+        planetName: "Mandalore",
+        description: "Once a glorious world, the surface of Mandalore was laid waste by war, leading Duchess Satine Kryze to renounce the planet’s violent history and declare neutrality in the Clone Wars.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/5/5e/Mandalore_SWCT.png/revision/latest/scale-to-width-down/1000?cb=20181015023000"
+    },
+    {
+        planetName: "Polis Massa",
+        description: "An obscure planetoid deep in the Outer Rim, Polis Massa nonetheless occupied an important place in galactic history: The Skywalker twins were born there and given names by their mother Padmé Amidala before she died. And plans made in secret on Polis Massa would one day determine the fate of the galaxy.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/d/dc/PolisMassa-BFII.jpg/revision/latest?cb=20090923124714"
+    },
+    {
+        planetName: "Corellia",
+        description: "Corellia was the capital planet of the Corellian system, which included Selonia, Drall, Tralus, and Talus.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/7/7f/Corellia_SOF.png/revision/latest/scale-to-width-down/1000?cb=20170222012701"
+    },
+    {
+        planetName: "Ryloth",
+        description: "Ryloth is the principal planet in the Ryloth system located in the Outer Rim near Tatooine. It is home to the Twi'leks, a humanoid species with two large, fleshy head-tails growing from their skulls.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/d/d3/Ryloth_EotECR.png/revision/latest/scale-to-width-down/1000?cb=20170222013022"
+    },
+    {
+        planetName: "Nal Hutta",
+        description: "A ringed, marshy bog planet controlled by the Hutts, Nal Hutta has long been a haven for criminal activity, far from the prying eyes of galactic authorities. Its hot atmosphere is frequently streaked by greasy rain, creating a fetid sauna in which Hutts are most comfortable.",
+        imgURL:"https://static.wikia.nocookie.net/starwars/images/0/0d/NalHutta-HFZ.png/revision/latest?cb=20130517035000"
+    },
+
+
+
+    ]
