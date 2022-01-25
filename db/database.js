036a9d16a64c6744d37d563bd92c4fc19582c656
Andrew Conte <andrew-conte434>
2022-01-25 14:17:51 -0500

    added heroku uri

--- db/database.js
+++ db/database.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize')
 const pkg = require('../package.json')
 
+const heroku = "postgres://sbhbkigdavdnyg:556298d0b0b7297c4cb0c679158c6cea269f5bc9507301030553411db0cda73c@ec2-3-224-157-224.compute-1.amazonaws.com:5432/db5d8gcn4jgmt9"
 const db = new Sequelize(
-    process.env.DATABASE || `postgres://postgres:sql123@localhost:5432/${pkg.name}`,
+    heroku || `postgres://postgres:sql123@localhost:5432/${pkg.name}`,
     {
         dialect:  'postgres',
         protocol: 'postgres',
