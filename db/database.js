db28d8de102951ceb9851f3426cfd58d51ce86c5
Andrew Conte <andrew-conte434>
2022-01-25 14:15:08 -0500

    changed DATABASE_URL to DATABASE

--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const pkg = require('../package.json')
 
 const db = new Sequelize(
-    process.env.DATABASE_URL || `postgres://postgres:sql123@localhost:5432/${pkg.name}`,
+    process.env.DATABASE || `postgres://postgres:sql123@localhost:5432/${pkg.name}`,
     {
         dialect:  'postgres',
         protocol: 'postgres',
