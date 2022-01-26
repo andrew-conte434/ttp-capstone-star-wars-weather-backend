912f90471d1b55b539b79f8cf9b16e6accc8dd61
Andrew Conte <andrew-conte434>
2022-01-26 15:28:03 -0500

    added columns to table

--- db/planets.js
+++ db/planets.js
@@ -1,3 +1,4 @@
+const { finalizeSession } = require('pg/lib/sasl')
 const Sequelize = require('sequelize')
 const db = require('./database')
 
@@ -18,6 +19,41 @@ const Planet = db.define('planet', {
     min_temp : {
         type : Sequelize.INTEGER,
         defaultValue : 273,
+        allowNull : false
+    },
+    max_temp : {
+        type : Sequelize.INTEGER,
+        defaultValue : 273,
+        allowNull : false
+    },
+    min_humidity : {
+        type : Sequelize.INTEGER,
+        defaultValue : 50,
+        allowNull : false
+    },
+    max_humidity : {
+        type : Sequelize.INTEGER,
+        defaultValue : 50,
+        allowNull : false
+    },
+    min_clouds : {
+        type : Sequelize.INTEGER,
+        defaultValue : 50,
+        allowNull : false
+    },
+    max_clouds : {
+        type : Sequelize.INTEGER,
+        defaultValue : 50,
+        allowNull : false
+    },
+    windy : {
+        type : Sequelize.BOOLEAN,
+        defaultValue : 50,
+        allowNull : false
+    },
+    fog : {
+        type : Sequelize.BOOLEAN,
+        defaultValue : false,
         allowNull : false
     }
 })
