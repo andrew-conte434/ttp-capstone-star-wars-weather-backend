caede994706e5cffc29fff2659135c94af6ac25c
Andrew Conte <andrew-conte434>
2022-01-26 15:50:03 -0500

    updated .put method

--- api/planets.js
+++ api/planets.js
@@ -39,7 +39,15 @@ router.put('/:id', async(req, res) => {
             .update({
                 name : req.body.name || planet.name,
                 description : req.body.description || planet.description,
-                imageUrl : req.body.imageUrl || planet.url,
+                imageUrl : req.body.imageUrl || planet.imageUrl,
+                min_temp : req.body.min_temp || planet.min_temp,
+                max_temp : req.body.max_temp || planet.max_temp,
+                min_humidity : req.body.min_humidity || planet.min_humidity,
+                max_humidity : req.body.max_humidity || planet.max_humidity,
+                min_clouds : req.body.min_clouds || planet.min_clouds,
+                max_clouds : req.body.max_clouds || planet.max_clouds,
+                windy : req.body.windy || planet.windy,
+                fog : req.body.fog || planet.fog
             },
             {where : {id : req.params.id}})
         res.status(200).send(`successfully updated ${planet.name}`)
